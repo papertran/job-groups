51d70bd109a58daa8b4ac02548dd66facde0ca78
Andres Ponciano <andres.ponciano123@gmail.com>
2020-05-20 23:32:03 -0400

    almost done with login page setup based on wireframe

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -7,26 +7,37 @@ class Login extends React.Component {
         <>
 
         <div class="flex h-screen inline">
-            <div class="bg-gray-500 w-3/5 h-full">
-                <h1 class="text-2xl font-mono font-bold p-20">Welcome to Job Groups!</h1>
-                <p class="font-mono px-20">
+            <div class="bg-mainpurple w-3/5 h-full">
+                
+                <h1 class="text-2xl font-mono font-bold pl-20 pt-12">
+                    <img class="inline" src={require('../images/Logo.png')} alt="idk"></img>
+                    <span class="pl-12">Welcome to Job Groups!</span>
+                </h1>
+                <p class="font-mono pl-20 pt-12 pr-12">
                     Job groups is a web application meant to help people search for jobs
                     by helping others find new job postings which are asfgsvsvsdvdsksdkfnsddPEE.
 
                     <br/>
 
-                    <img class="pt-20 pl-64 h-64" src={require('../images/clumsy.png')} alt="notfsadasduppypng"></img>
+                    <img class="pl-48 h-auto" src={require('../images/clumsy_w_jobs.png')} alt="notfsadasduppypng"></img>
                 </p>
-                {/* <img class="p-20 object-contain" src={require('../images/clumsy.png')} alt="notfsadasduppypng"></img> */}
+                {/* <img class="pt-8 pl-56 h-auto" src={require('../images/clumsy_w_jobs.png')} alt="notfsadasduppypng"></img> */}
             </div>
 
-            <div class="bg-gray-200 w-2/5 h-full">
+            <div class="bg-secpurple w-2/5 h-full">
 
-                <form class="w-full max-w-sm p-32">
+                <form class="w-full max-w-lg items-center pl-48 pt-40">
 
-                    <div class="mb-4">
-                        Username or email
-                        <input class=""></input>
+                    <div class="flex items-center w-3/4 border-b border-darkpurple py-2">
+                        <input class="appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none w-3/4" type="text" placeholder="Username or email"></input>
+                    </div>
+
+                    <div class="flex items-center w-3/4 border-b border-darkpurple py-2 pt-10">
+                        <input class="appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none w-3/4" type="text" placeholder="Password"></input>
+                    </div>
+
+                    <div class="flex items-center justify-between w-3/4 py-2 pt-10">
+                        <button class="bg-darkpurple2 hover:bg-secpurple text-white rounder" type="button">Sign in</button>
                     </div>
 
                 </form>
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'mainpurple': '#9EB3FC',
+        'secpurple': '#B5C4FA',
+        'darkpurple': '#2A2880',
+        'darkpurple2': '#5A58C6',
+        'regal-blue': '#243c5a',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
