57a9f84a04a6be3cab94a26eeb70e4afea465c14
Kevin <kevintrn99@gmail.com>
2020-06-13 13:54:11 -0400

    refractored router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
-import './App.css';
-import Login from './components/Login';
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 class App extends React.Component {
-
-  render () {
-    return (
-      <>
-        <Login/>
-      </>
-    )
-  }
+	render() {
+		return (
+			<>
+				<Switch>
+					<Route exact path="/" component={Login} />
+					<Route path="/Home" component={Home} />
+					<Route component={NotFound} />
+				</Switch>
+			</>
+		);
+	}
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Login from './components/Login';
-import App from './App';
-import Router from './components/Router';
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-render(<Router />, document.querySelector('#root'));
+render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.querySelector("#root")
+);
