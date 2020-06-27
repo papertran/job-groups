63092680ba1da1fcbc0e3106e7754def1e87cd9e
Andres Ponciano <andres.ponciano123@gmail.com>
2020-06-27 16:06:11 -0400

    setup username list for group

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,10 +3,9 @@ import React from 'react';
 export const Header = () => {
     return (
         <>
-            <div className="flex justify-between w-full h-24 bg-darkpurple">
-            <img className="inline h-16 mt-4 ml-12" src={require('../images/Logo.png')} alt="idk"></img>
-            <img className="inline rounded-full h-16 mt-4 mr-12" src={require('../logo.svg')} alt="idk"></img>
-                
+            <div className="flex justify-between w-full h-20 bg-darkpurple">
+            <img className="inline h-12 mt-4 ml-12" src={require('../images/Logo.png')} alt="idk"></img>
+            <img className="inline rounded-full h-12 mt-4 mr-12" src={require('../logo.svg')} alt="idk"></img>
             </div>
         </>
     )
