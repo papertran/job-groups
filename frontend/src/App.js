55d57fe7f66a563e7c542383696483e2ef12a20a
Kevin <kevintrn99@gmail.com>
2020-07-11 12:22:08 -0400

    fixed some missing files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import "./App.css";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import NotFound from "./components/NotFound";
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 export const Header = () => {
-    return (
-        <>
-            <div className="flex justify-between w-full h-20 bg-darkpurple">
-            <img className="inline h-12 mt-4 ml-12" src={require('../images/Logo.png')} alt="idk"></img>
-            <img className="inline rounded-full h-12 mt-4 mr-12" src={require('../logo.svg')} alt="idk"></img>
-            </div>
-        </>
-    )
-}
+	return (
+		<>
+			<div className="flex justify-between w-full h-20 bg-darkpurple">
+				{/* <img className="inline h-12 mt-4 ml-12" src={require('../images/Logo.png')} alt="idk"></img> */}
+				{/* <img className="inline rounded-full h-12 mt-4 mr-12" src={require('../logo.svg')} alt="idk"></img> */}
+			</div>
+		</>
+	);
+};
