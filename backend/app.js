1aefbc1dd7bdf9dd56e8bcf620e49cd10718b6d9
Kevin <kevintrn99@gmail.com>
2020-07-17 21:11:55 -0400

    basic setup of app.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 
 const app = express();
 
 mongoose.Promise = global.Promise;
 
-//
+// This is to be able to run mocha tests
 if ((process.env, NODE_ENV !== 'test')) {
-    mongoose.connect('mongodb://localhost/job_groups');
+    mongoose.connect(keys.mongoURI);
 }
+
+app.use(bodyParser);
+
+module.exports = app;
