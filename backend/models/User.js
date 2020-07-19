232c14f46f3153a3273d3b3d2ea23a7a98e1ca1f
Kevin <kevintrn99@gmail.com>
2020-07-19 15:03:00 -0400

    updated user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    name: String,
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
     googleId: String,
     group: [
         {
