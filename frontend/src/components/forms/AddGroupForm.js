55abe1b3fb5802a0218c54b274fa2d6156c298e2
Andres Ponciano <andres.ponciano123@gmail.com>
2020-08-16 16:26:33 -0400

    styled rest of forma

--- frontend/src/components/forms/AddGroupForm.js
+++ frontend/src/components/forms/AddGroupForm.js
@@ -4,9 +4,13 @@ import { Field, reduxForm } from 'redux-form';
 class AddGroupForm extends Component {
     renderInput = ({ input, label, meta }) => {
         return (
-            <div>
-                <label>{label}</label>
-                <input {...input} autoComplete="off" className="border " />
+            <div className="flex mt-10 mx-8 justify-between">
+                <label className="mr-1">{label}</label>
+                <input
+                    {...input}
+                    autoComplete="off"
+                    className="shadow appearance-none border rounded px-2 py-1 text-gray-700 focus:outline-none focus:shadow-outline"
+                />
             </div>
         );
     };
@@ -17,19 +21,27 @@ class AddGroupForm extends Component {
 
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-                <Field
-                    name="groupName"
-                    component={this.renderInput}
-                    label="Enter Group Name"
-                />
-                <Field
-                    name="description"
-                    component={this.renderInput}
-                    label="Enter Description"
-                />
-                <button className="border">Submit</button>
-            </form>
+            <div className="bg-secpurple h-screen flex flex-col justify-center items-center">
+                <h1 className="text-2xl text-gray-700 mb-4">Create a group</h1>
+                <form
+                    className="bg-darkpurple2 h-64 flex-shrink-0 flex flex-col rounded-lg text-white"
+                    onSubmit={this.props.handleSubmit(this.onSubmit)}
+                >
+                    <Field
+                        name="groupName"
+                        component={this.renderInput}
+                        label="Enter Group Name"
+                    />
+                    <Field
+                        name="description"
+                        component={this.renderInput}
+                        label="Enter Description"
+                    />
+                    <button className="border self-center rounded mt-12 bg-mainpink w-1/4 h-10">
+                        Submit
+                    </button>
+                </form>
+            </div>
         );
     }
 }
--- frontend/src/components/forms/AddJobForm.js
+++ frontend/src/components/forms/AddJobForm.js
@@ -4,9 +4,13 @@ import { Field, reduxForm } from 'redux-form';
 class AddJobForm extends Component {
     renderInput = ({ input, label, meta }) => {
         return (
-            <div>
-                <label>{label}</label>
-                <input {...input} autoComplete="off" className="border " />
+            <div className="flex mt-10 mx-8 justify-between">
+                <label className="mr-1">{label}</label>
+                <input
+                    {...input}
+                    autoComplete="off"
+                    className="shadow appearance-none border rounded px-2 py-1 text-gray-700 focus:outline-none focus:shadow-outline"
+                />
             </div>
         );
     };
@@ -17,39 +21,47 @@ class AddJobForm extends Component {
 
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-                <Field
-                    name="groupName"
-                    component={this.renderInput}
-                    label="Enter Group Name"
-                />
-                <Field
-                    name="jobName"
-                    component={this.renderInput}
-                    label="Enter Job Name"
-                />
-                <Field
-                    name="position"
-                    component={this.renderInput}
-                    label="Enter Job Position"
-                />
-                <Field
-                    name="url"
-                    component={this.renderInput}
-                    label="Enter Job url"
-                />
-                <Field
-                    name="location"
-                    component={this.renderInput}
-                    label="Enter Job Location"
-                />
-                <Field
-                    name="pay"
-                    component={this.renderInput}
-                    label="Enter Job Pay"
-                />
-                <button className="border">Submit</button>
-            </form>
+            <div className="bg-secpurple h-screen flex flex-col justify-center items-center">
+                <h1 className="text-2xl text-gray-700 mb-4">Add a Job</h1>
+                <form
+                    className="bg-darkpurple2 flex-shrink-0 flex flex-col rounded-lg text-white"
+                    onSubmit={this.props.handleSubmit(this.onSubmit)}
+                >
+                    <Field
+                        name="groupName"
+                        component={this.renderInput}
+                        label="Enter Group Name"
+                    />
+                    <Field
+                        name="jobName"
+                        component={this.renderInput}
+                        label="Enter Job Name"
+                    />
+                    <Field
+                        name="position"
+                        component={this.renderInput}
+                        label="Enter Job Position"
+                    />
+                    <Field
+                        name="url"
+                        component={this.renderInput}
+                        label="Enter Job url"
+                    />
+                    <Field
+                        name="location"
+                        component={this.renderInput}
+                        label="Enter Job Location"
+                    />
+                    <Field
+                        name="pay"
+                        component={this.renderInput}
+                        label="Enter Job Pay"
+                    />
+                    <button className="border self-center rounded my-6 bg-mainpink w-1/4 h-10">
+                        Submit
+                    </button>
+                </form>
+            </div>
         );
     }
 }
